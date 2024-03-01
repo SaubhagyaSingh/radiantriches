824063df9b35c70b0b999bf0db539605072f7b87
Saubhagya <saubhagyasingh65@gmail.com>
2024-03-02 00:23:15 +0530

    FIX2

--- src/components/breadcrumbs.jsx
+++ src/components/breadcrumbs.jsx
@@ -1,34 +1,30 @@
 import React from "react";
 import { FiChevronDown } from "react-icons/fi";
-import { GoInfo } from "react-icons/go";
-import { GoVerified } from "react-icons/go";
+import { GoInfo, GoVerified } from "react-icons/go";
 
 const Breadcrumbs = () => {
   return (
     <div className="mt-[11px]">
-      <hr className="bg-#E1E4E6 w-[1024px] ml-[244px] p-2"></hr>
-      <div
-        className="flex items-center gap-8 ml-[250px] text-[#4B5665] 
-"
-      >
+      <hr className="bg-gray-300 w-[1024px] ml-[244px] p-2" />
+      <div className="flex items-center gap-8 ml-[250px] text-[#4B5665]">
         <div className="flex flex-row">
-          <GoVerified size={18} className="mt-1 mr-1"></GoVerified>
+          <GoVerified size={18} className="mt-1 mr-1" />
           <span>Last Updated - </span>
         </div>
-        <span>February 22,2020</span>
+        <span>February 22, 2020</span>
 
-        <div class="flex flex-row">
+        <div className="flex flex-row">
           <GoInfo size={18} className="mt-1 mr-1" />
           <span>Disclosure</span>
         </div>
-        <div class="ml-[490px]  flex flex-row items-center">
+        <div className="ml-[490px] flex flex-row items-center">
           <span>Relevant</span>
           <span className="mt-1">
             <FiChevronDown />
           </span>
         </div>
       </div>
-      <hr className="#E1E4E6 w-[1024px] ml-[244px] mt-4"></hr>
+      <hr className="bg-gray-300 w-[1024px] ml-[244px] mt-4" />
     </div>
   );
 };

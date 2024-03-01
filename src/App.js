36afe486adf66896bccccf620d91c1b6be57e451
Saubhagya <saubhagyasingh65@gmail.com>
2024-03-01 23:46:46 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Footer from "./components/Footer";
-
+import Home from "./pages/Home";
 function App() {
   return (
     <div>
